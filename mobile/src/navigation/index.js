1e686073f4d827a993e7087a5169397ab5cb8c56
Madhav Gill <221120928+manav-gill@users.noreply.github.com>
2026-04-05 00:39:18 +0530

    refactor: remove StackNavigator and redirect to TabNavigator

--- mobile/src/navigation/index.js
+++ mobile/src/navigation/index.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TestScreen from '../screens/TestScreen';
-
-const Stack = createNativeStackNavigator();
-
-export default function AppNavigator() {
-  return (
-    <Stack.Navigator initialRouteName="TestScreen">
-      <Stack.Screen
-        name="TestScreen"
-        component={TestScreen}
-        options={{ title: 'PortfolioPilot Mobile' }}
-      />
-    </Stack.Navigator>
-  );
-}
+export { default } from './TabNavigator';
